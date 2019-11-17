0f177e9a54ff9c312c01208d41ea2caa45a22691
InLine <fernando.gmp@gmail.com>
2019-11-17 10:20:50 -0300

    adicionando método para transmitir mensagem para telegram

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import request, jsonify
+import urllib2
 
 app = Flask(__name__)
+base_url = 'https://api.telegram.org/'
 
-@app.route("/hello")
-def hello():
-	return "Hello World!"
+@app.route("/testSignal/<bot>/<chat_id>/<msg>", methods=['GET', 'POST'])
+def broadcast(bot, chat_id, msg):
+	if request.method == 'GET':
+		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
+		return contents
+	if request.method == 'POST':
+		bot = request.form.get('bot')
+        	chat_id = request.form.get('chat_id')
+        	msg = request.form.get('msg')
+		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
+		return jsonify(contents)
 
 if __name__ == '__main__':
 	app.run(debug=True)
