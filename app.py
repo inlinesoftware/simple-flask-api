2c8a1c704acf67c1f77290ba2d8075ba384d0c5a
InLine <fernando.gmp@gmail.com>
2019-11-18 17:55:05 -0300

    implementando token

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 from flask import Flask
 from flask import request, jsonify
 import urllib2
+import json
 
 app = Flask(__name__)
 base_url = 'https://api.telegram.org/'
+TOKEN = '1234'
 
-@app.route("/testSignal/<bot>/<chat_id>/<msg>", methods=['GET', 'POST'])
-def broadcast(bot, chat_id, msg):
-	if request.method == 'GET':
-		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
-		return contents
-	if request.method == 'POST':
-		bot = request.form.get('bot')
-        	chat_id = request.form.get('chat_id')
-        	msg = request.form.get('msg')
-		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
-		return jsonify(contents)
+def authorize(_token):
+	if _token == TOKEN:
+		return True
+	else:
+		return False
+
+@app.route("/testSignal/<bot>/<chat_id>/<msg>", methods=['GET'])
+def broadcast_get(bot, chat_id, msg):
+	contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
 
 @app.route("/broadcast", methods=['POST'])
-def broad():
+def broadcast_post():
 	data = request.json
-	return jsonify(data)
+	json_str = json.dumps(data)
+	resp = json.loads(json_str)
+	_token, bot, chat_id, msg = resp['token'], resp['bot'], resp['chat_id'], resp['msg']
+	if authorize(_token):
+		contents = urllib2.urlopen(base_url+bot+"/sendMessage?chat_id="+chat_id+"&text="+msg).read()
+	else:
+		return "invalid token"
 
 if __name__ == '__main__':
 	app.run(debug=True)
